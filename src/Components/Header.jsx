00319dd27c9d6a39651507549a83522a44f7edb6
SuMFeRa <Beserith1121@gmail.com>
2022-07-27 14:12:05 +0300

    Adding new files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://kartinkin.net/uploads/posts/2021-07/thumbs/1626314476_50-kartinkin-com-p-fon-dlya-emblemi-krasivo-50.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q= "
+                    alt=""/>
+            </div>
+            <div>
+                ava + descripyion
+                <img src="" alt=""/>
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
